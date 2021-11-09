2ba5bfd7ccbe55d70d9c1bfba84a5889b8b94bdf
lordfoom <timothys@soliditech.com>
2021-11-09 21:07:48 +0200

    Remove commented out code

--- src/db.rs
+++ src/db.rs
@@ -30,25 +30,6 @@ pub fn save_list(list: &RutuduList) -> Result<(), Box<dyn Error>> {
 
            }})
         });
-    // for item in &list.items.items{
-    //     match conn.execute("INSERT INTO rutudu_list(id, parent_id, title, entry, completeStatus, expandStatus , create_date)
-    //                                 VALUES(?1, ?2, ?3, ?4, ?5, ?6, strftime('%Y-%m-%d %H-%M-%S','now') )",
-    //                        params![&item.id, &item.parent_id, &item.title, &item.entry, &item.complete.to_u8(), &item.expand.to_u8()]){
-    //         Ok(updated) => debug!("Number of rows inserted: {}", updated),
-    //         Err(why) => error!("Failed to insert row: {}", why),
-    //     }
-    // }
-    // list.items
-    //     .items
-    //     .iter()
-    //     .map(|i| {
-    //         debug!("About to run the execute");
-    //         match conn.execute("INSERT INTO rutudu_list(parent_id, title, entry, create_date)
-    //                                 VALUES(?1,?2,?3,now()", params![0, &i.title, &i.entry]){
-    //             Ok(updated) => debug!("Number of rows inserted: {}", updated),
-    //             Err(why) => error!("Failed to insert row: {}", why),
-    //         }
-    //     });
     Ok(())
 }
 
